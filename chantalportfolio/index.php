5d5f1c143a4410efdfff4d5e0a567af13a3ad79b
Eric Robinson <eric.z.robinson@gmail.com>
2014-09-10 00:19:52 -0400

    changes #2

--- chantalportfolio/index.php
+++ chantalportfolio/index.php
@@ -1,202 +1,240 @@
 <!DOCTYPE html>
 <!--[if lte IE 8]>              <html class="ie8 no-js" lang="en">     <![endif]-->
 <!--[if IE 9]>					<html class="ie9 no-js" lang="en">     <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--> <html class="not-ie no-js" lang="en">  <!--<![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class="not-ie no-js" lang="en">
+<!--<![endif]-->
+
 <head>
-	<!-- Google Web Fonts
+    <!-- Google Web Fonts
   ================================================== -->
-	<link href='http://fonts.googleapis.com/css?family=Allura|Oswald:400,700,300' rel='stylesheet' type='text/css'>
-	
-	<!-- Basic Page Needs
+    <link href='http://fonts.googleapis.com/css?family=Allura|Oswald:400,700,300' rel='stylesheet' type='text/css'>
+
+    <!-- Basic Page Needs
   ================================================== -->
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <!--[if ie]><meta http-equiv='X-UA-Compatible' content="IE=edge,IE=9,IE=8,chrome=1" /><![endif]-->
-	
-	<title>Chantal Paquin | Interiors</title>
-	
-	<meta name="description" content="">
-	<meta name="author" content="">
-	<link rel="shortcut" href="images/favicon.ico" />
-	
-	<!-- Mobile Specific Metas
+
+    <title>Chantal Paquin | Interiors</title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut" href="images/favicon.ico" />
+
+    <!-- Mobile Specific Metas
   ================================================== -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	
-	<!-- CSS
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
   ================================================== -->
-	<link rel="stylesheet" href="css/style.css" />
-	<link rel="stylesheet" href="css/skeleton.css" />
-	<link rel="stylesheet" href="css/layout.css" />
-	
-	<link rel="stylesheet" href="css/font-awesome.css" />
-	<link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/skeleton.css" />
+    <link rel="stylesheet" href="css/layout.css" />
+
+    <link rel="stylesheet" href="css/font-awesome.css" />
+    <link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" />
     <link rel="stylesheet" href="js/epicslider/epicslider-responsive.css" />
 
-	<!-- HTML5 Shiv
+    <!-- HTML5 Shiv
 	================================================== -->
-	<script src="js/jquery.modernizr.js"></script>
-	
+    <script src="js/jquery.modernizr.js"></script>
+
 </head>
+
 <body>
-	
-<!-- - - - - - - - - - - - - - Header - - - - - - - - - - - - - - - - -->	
 
-<?php include('menu.php'); ?>
+    <!-- - - - - - - - - - - - - - Header - - - - - - - - - - - - - - - - -->
 
-<!-- - - - - - - - - - - - - - end Header - - - - - - - - - - - - - - - - -->	
+    <?php include( 'menu.php'); ?>
 
-
-<!-- - - - - - - - - - - - - - - - Content - - - - - - - - - - - - - - - - -->	
-
-<div id="wrapper" class="sbr">
-	
-	<div id="content">
-
-		<div class="scroll-box">
-
-			<div class="grid">
-
-				<div class="gr-box">
-
-					<div class="item full">
-
-						<figure class="gr-figure">
-							<a class="gr-lightbox single-image plus-icon animTop" href="BlizzardEntertainment.html" rel="grid">
-								<img src="images/Blizzard/BlizzardWorkStation_big.jpg" alt="">
-							</a>
-
-							<a href="BlizzardEntertainment.html" class="gr-caption">
-								<h5>Blizzard Entertainment</h5>
-								<span>Office</span>
-							</a>	
-						</figure><!--/ .gr-figure-->
-
-					</div><!--/ .item-->
-
-				</div><!--/ .gr-box-->
-
-				<div class="gr-box">
-
-					<div class="item full">
-
-						<figure class="gr-figure">
-							<a class="gr-lightbox single-image plus-icon animTop" href="Douglas.html" rel="grid">
-								<img src="images/Douglas/Douglas_Big.jpg" alt="">
-							</a>
-
-							<a href="Douglas.html" class="gr-caption">
-								<h5>Douglas Project</h5>
-								<span>House Renovation</span>
-							</a>	
-						</figure><!--/ .gr-figure-->
-
-					</div><!--/ .item-->
-
-				</div><!--/ .gr-box-->
-
-				<div class="gr-box">
-
-					<div class="item full">
-
-						<figure class="gr-figure">
-							<a class="gr-lightbox single-image plus-icon animTop" href="MulberryCommunity.html" rel="grid">
-								<img src="images/Mulberry/Checkers_Big.jpg" alt="">
-							</a>
-
-							<a href="MulberryCommunity.html" class="gr-caption">
-								<h5>Mulberry Community</h5>
-								<span>For Individuals With Intellectual Diversities</span>
-							</a>	
-						</figure><!--/ .gr-figure-->
-
-					</div><!--/ .item-->
-
-				</div><!--/ .gr-box-->
-
-				<div class="gr-box">
-
-					<div class="item full">
-
-						<figure class="gr-figure">
-							<a class="gr-lightbox single-image plus-icon animTop" href="HandRenderings.html" rel="grid">
-								<img src="images/HandRendering/BookRendering_Big.jpg" alt="">
-							</a>
-
-							<a href="HandRenderings.html" class="gr-caption">
-								<h5>Hand Renderings</h5>
-								<span>Book / Magazines</span>
-							</a>	
-						</figure><!--/ .gr-figure-->
-
-					</div><!--/ .item-->
-
-				</div><!--/ .gr-box-->
-
-			</div><!--/ .grid-->
-
-		</div><!--/ .scroll-box-->
-
-	</div><!--/ #content-->
-	
-</div><!--/ #wrapper-->
-
-<!-- - - - - - - - - - - - - - - - Content - - - - - - - - - - - - - - - - -->	
+    <!-- - - - - - - - - - - - - - end Header - - - - - - - - - - - - - - - - -->
 
 
-<!-- - - - - - - - - - - - - - - Footer - - - - - - - - - - - - - - - - -->	
+    <!-- - - - - - - - - - - - - - - - Content - - - - - - - - - - - - - - - - -->
 
-<footer id="footer">
+    <div id="wrapper" class="sbr">
 
-	<div class="container">
+        <div id="content">
 
-		<div class="sixteen columns">
+            <div class="scroll-box">
 
-			<div class="eight columns alpha">
+                <div class="grid">
 
-				<div class="copyright">
-					Copyright &copy; 2013. ThemeMakers. All rights reserved
-				</div><!--/ .copyright-->
+                    <div class="gr-box">
 
-				<div class="developed">
-					Developed by <a href="#">ThemeMakers</a>
-				</div><!--/ .developed-->
+                        <div class="item full">
 
-			</div><!--/ .columns-->
+                            <figure class="gr-figure">
+                                <a class="gr-lightbox single-image plus-icon animTop" href="BlizzardEntertainment.php" rel="grid">
+                                    <img src="images/Blizzard/BlizzardWorkStation_big.jpg" alt="">
+                                </a>
 
-			<div class="eight columns omega">
+                                <a href="BlizzardEntertainment.php" class="gr-caption">
+                                    <h5>Blizzard Entertainment</h5>
+                                    <span>Office</span>
+                                </a>
+                            </figure>
+                            <!--/ .gr-figure-->
 
-				<ul class="social-icons">
-					<li class="twitter"><a target="_blank" href="https://twitter.com/ThemeMakers"><span>Twitter</span></a></li>
-					<li class="facebook"><a target="_blank" href="http://www.facebook.com/wpThemeMakers"><span>Facebook</span></a></li>
-					<li class="dribble"><a target="_blank" href="http://dribbble.com/"><span>Dribbble</span></a></li>
-					<li class="vimeo"><a target="_blank" href="https://vimeo.com/"><span>Vimeo</span></a></li>
-					<li class="youtube"><a target="_blank" href="http://www.youtube.com/"><span>Youtube</span></a></li>
-					<li class="instagram"><a target="_blank" href="http://www.instagram.com/"><span>Instagram</span></a></li>
-					<li class="rss"><a target="_blank" href="#"><span>Rss</span></a></li>
-				</ul><!--/ .social-icons-->	
+                        </div>
+                        <!--/ .item-->
 
-			</div><!--/ .columns-->
+                    </div>
+                    <!--/ .gr-box-->
 
-		</div><!--/ .columns-->
+                    <div class="gr-box">
 
-	</div><!--/ .container-->
+                        <div class="item full">
 
-</footer><!--/ #footer-->
+                            <figure class="gr-figure">
+                                <a class="gr-lightbox single-image plus-icon animTop" href="Douglas.php" rel="grid">
+                                    <img src="images/Douglas/Douglas_Big.jpg" alt="">
+                                </a>
 
-<!-- - - - - - - - - - - - - - end Footer - - - - - - - - - - - - - - - -->			
+                                <a href="Douglas.php" class="gr-caption">
+                                    <h5>Douglas Project</h5>
+                                    <span>House Renovation</span>
+                                </a>
+                            </figure>
+                            <!--/ .gr-figure-->
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
-<script src="js/jquery.easing.1.3.min.js"></script>
-<script src="js/jquery.cycle.all.min.js"></script>
-<script src="js/jquery.mousewheel.js"></script>
-<script src="js/jquery.nicescroll.js"></script>
-<!--For Touch Devices-->
-<script src="js/jquery.touchswipe.min.js"></script>
-<!--end Touch Devices-->
-<script src="js/fancybox/jquery.fancybox.pack.js"></script>
-<script src="js/jquery.resizegrid.js"></script>
-<script src="js/config.js"></script>
-<script src="js/custom.js"></script>
+                        </div>
+                        <!--/ .item-->
+
+                    </div>
+                    <!--/ .gr-box-->
+
+                    <div class="gr-box">
+
+                        <div class="item full">
+
+                            <figure class="gr-figure">
+                                <a class="gr-lightbox single-image plus-icon animTop" href="MulberryCommunity.php" rel="grid">
+                                    <img src="images/Mulberry/Checkers_Big.jpg" alt="">
+                                </a>
+
+                                <a href="MulberryCommunity.php" class="gr-caption">
+                                    <h5>Mulberry Community</h5>
+                                    <span>For Individuals With Intellectual Diversities</span>
+                                </a>
+                            </figure>
+                            <!--/ .gr-figure-->
+
+                        </div>
+                        <!--/ .item-->
+
+                    </div>
+                    <!--/ .gr-box-->
+
+                    <div class="gr-box">
+
+                        <div class="item full">
+
+                            <figure class="gr-figure">
+                                <a class="gr-lightbox single-image plus-icon animTop" href="HandRenderings.php" rel="grid">
+                                    <img src="images/HandRendering/BookRendering_Big.jpg" alt="">
+                                </a>
+
+                                <a href="HandRenderings.php" class="gr-caption">
+                                    <h5>Hand Renderings</h5>
+                                    <span>Book / Magazines</span>
+                                </a>
+                            </figure>
+                            <!--/ .gr-figure-->
+
+                        </div>
+                        <!--/ .item-->
+
+                    </div>
+                    <!--/ .gr-box-->
+
+                </div>
+                <!--/ .grid-->
+
+            </div>
+            <!--/ .scroll-box-->
+
+        </div>
+        <!--/ #content-->
+
+    </div>
+    <!--/ #wrapper-->
+
+    <!-- - - - - - - - - - - - - - - - Content - - - - - - - - - - - - - - - - -->
+
+
+    <!-- - - - - - - - - - - - - - - Footer - - - - - - - - - - - - - - - - -->
+
+    <footer id="footer">
+
+        <div class="container">
+
+            <div class="sixteen columns">
+
+                <div class="eight columns alpha">
+
+                    <div class="copyright">
+                        Copyright &copy; 2013. ThemeMakers. All rights reserved
+                    </div>
+                    <!--/ .copyright-->
+
+                    <div class="developed">
+                        Developed by <a href="#">ThemeMakers</a>
+                    </div>
+                    <!--/ .developed-->
+
+                </div>
+                <!--/ .columns-->
+
+                <div class="eight columns omega">
+
+                    <ul class="social-icons">
+                        <li class="twitter"><a target="_blank" href="https://twitter.com/ThemeMakers"><span>Twitter</span></a>
+                        </li>
+                        <li class="facebook"><a target="_blank" href="http://www.facebook.com/wpThemeMakers"><span>Facebook</span></a>
+                        </li>
+                        <li class="dribble"><a target="_blank" href="http://dribbble.com/"><span>Dribbble</span></a>
+                        </li>
+                        <li class="vimeo"><a target="_blank" href="https://vimeo.com/"><span>Vimeo</span></a>
+                        </li>
+                        <li class="youtube"><a target="_blank" href="http://www.youtube.com/"><span>Youtube</span></a>
+                        </li>
+                        <li class="instagram"><a target="_blank" href="http://www.instagram.com/"><span>Instagram</span></a>
+                        </li>
+                        <li class="rss"><a target="_blank" href="#"><span>Rss</span></a>
+                        </li>
+                    </ul>
+                    <!--/ .social-icons-->
+
+                </div>
+                <!--/ .columns-->
+
+            </div>
+            <!--/ .columns-->
+
+        </div>
+        <!--/ .container-->
+
+    </footer>
+    <!--/ #footer-->
+
+    <!-- - - - - - - - - - - - - - end Footer - - - - - - - - - - - - - - - -->
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')
+    </script>
+    <script src="js/jquery.easing.1.3.min.js"></script>
+    <script src="js/jquery.cycle.all.min.js"></script>
+    <script src="js/jquery.mousewheel.js"></script>
+    <script src="js/jquery.nicescroll.js"></script>
+    <!--For Touch Devices-->
+    <script src="js/jquery.touchswipe.min.js"></script>
+    <!--end Touch Devices-->
+    <script src="js/fancybox/jquery.fancybox.pack.js"></script>
+    <script src="js/jquery.resizegrid.js"></script>
+    <script src="js/config.js"></script>
+    <script src="js/custom.js"></script>
 </body>
+
 </html>
